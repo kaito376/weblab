ed1b47d1608855836cd8ecbfc7d2ccb46c88f619
kaito376 <c0b2215358@gmail.com>
2025-05-06 23:42:25 +0900

    基本的なページを作成しました

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <h1>テスト</h1>
+      <BrowserRouter>
+        <div className="container">
+          <nav>
+            <ul>
+              <li>
+                <Link to="/weblab">ホーム</Link>
+                <Link to="/about">About</Link>
+                <Link to="/contact">Contact</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/weblab" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </>
   );
 }
